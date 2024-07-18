57828a6361e6c091605c25a98278937e1538d432
lilacilla <149900581+lilacilla@users.noreply.github.com>
2024-07-18 21:45:24 +0100

    Update script.js

--- src/script.js
+++ src/script.js
@@ -24,7 +24,7 @@ function generateRecipe(event) {
   let apiKey = "4c8c7b09546det2d72f3521aa5o882ea";
 
   let prompt = `Give a recipe containing the ingredients/keywords given to you here: ${searchInput.value}`;
-  let context = `provide a recipe containing information provided in ${prompt}. Use html tags to display it better and separate each line with a html <br /> or <li></li> tag if possible. Do not override CSS styling. This is important, recipe titles must always display between a h2 tag, example <h2>Raspberry Lemonade</h2>, subtitles must always display between a h3 tag, example <h3>Ingredients</h3>, do not ignore. Provide serving sizes in h4 tags. Prioritise celsius.`;
+  let context = `Provide a recipe containing information provided in ${prompt}. Use html tags to display it better and separate each line with a html <br /> or <li></li> tag if possible. Do not override CSS styling. This is important, recipe titles must always display between a h2 tag, example <h2>Raspberry Lemonade</h2>, subtitles must always display between a h3 tag, example <h3>Ingredients</h3>, do not ignore. Always provide ingredients. Always provide serving sizes in h4 tags, do not ignore. Always provide instructions. Prioritise celsius.`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   axios.get(apiUrl).then(displayRecipe);
